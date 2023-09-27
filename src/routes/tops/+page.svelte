cfbfca74ee327b74d8ee0e6352fc7bcd3c05cfb3
Harris Dzulkarnain <harisidzwan@gmail.com>
2023-09-27 16:21:31 +0800

    update

--- src/routes/tops/+page.svelte
+++ src/routes/tops/+page.svelte
@@ -1,29 +1,47 @@
-    <script>
-      import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public'
-      import { getTokenFromLocalStorage } from '../../lib/auth.js';
-      import { goto } from '$app/navigation';
-      export let data;
+<script>
+  import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public'
+  import { getTokenFromLocalStorage } from '../../lib/auth.js';
+  import { goto } from '$app/navigation';
+  export let data;
     
       
-      export function selectTop(tops) {
+  export function selectTop(tops) {
         sessionStorage.clear
         sessionStorage.setItem("tops", tops)
         console.log(tops)
       }
 
-      // const top = sessionStorage.getItem()
+      let filtering = false;
 
-      // Modify the selectTop function to store the selected top in session storage
-  // export function selectTop(tops) {
-  // Store the selected top in session storage
-    // sessionStorage.setItem("selectedTop", JSON.stringify(tops));
+// Define a variable to store the filtered tops
+let filteredTops = [];
 
-  // Navigate to the new page
-    // goto('/outfit');
-// }
+// Define a variable to keep track of the selected color
+let selectedColor = '';
 
 
-  </script>
+
+// Function to filter tops by color
+function filterByColor() {
+  if (selectedColor === '') {
+    // If no color is selected, reset the filter to show all tops
+    filtering = false;
+    filteredTops = [];
+  } else {
+    // If a color is selected, filter the tops by that color
+    filtering = true;
+    filteredTops = data.tops.filter(tops => tops.color === selectedColor);
+  }
+}
+</script>
+
+<select bind:value={selectedColor}>
+  <option value="">All Colors</option>
+  <option value="white">White</option>
+  <option value="black">Black</option>
+  <option value="blue">Blue</option>
+  <!-- Add more color options as needed -->
+</select>
 
 
 
@@ -32,11 +50,12 @@
       Upload New Top
     </a>
 
+    <button class="btn" on:click={filterByColor}>Apply Filter</button>
 
-<div id="tops-list" class="flex flex-wrap">
-  
-  {#each data.tops as tops}
-      <div class="border border-gray-300 p-4 m-4 text-center flex-shrink-0">
+
+    <div id="tops-list" class="flex flex-wrap">
+      {#each (filtering ? filteredTops : data.tops) as tops}
+        <div class="border border-gray-300 p-4 m-4 text-center flex-shrink-0">
         <button class="btn" on:click={() => selectTop(tops.name)}>Select</button>
           <a class="font-bold text-2xl" href="/tops/{tops.id}">{tops.name}</a>
           <img class="max-w-xs max-h-xs mx-auto mb-2" src={tops.url} alt={tops.url} />
