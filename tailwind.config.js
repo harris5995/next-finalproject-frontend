9ce2e9e7a037226be932adb34428cc94e645a159
Shane Capri <shanecaprichin@gmail.com>
2023-09-30 02:04:27 +0800

    rechecked daisyui themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,32 +6,8 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-        {
-          mytheme: {
-          
- "primary": "#d1c1d7",
-          
- "secondary": "#ffe4e6",
-          
- "accent": "#b4e9d6",
-          
- "neutral": "#70acc7",
-          
- "base-100": "#ffffff",
-          
- "info": "#3abff8",
-          
- "success": "#36d399",
-          
- "warning": "#fbbd23",
-          
- "error": "#f87272",
-          },
-        },
-      ],
-    },
-  }
-    // themes: ["autumn", "coffee"],
-    // darkTheme: "coffee",
-    
+    themes: ["autumn", "coffee"],
+    darkTheme: "coffee",
+    lightTheme: "autumn",
+        }
+      }
